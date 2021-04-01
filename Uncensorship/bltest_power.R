cf294b279b112010fe04faaa160f2c555af6bed5
beautifulstatistics <wayneleokenney@gmail.com>
2021-04-01 14:10:12 +0000

    more power

--- Uncensorship/bltest_power.R
+++ Uncensorship/bltest_power.R
@@ -4,9 +4,9 @@ library(brms)
 options(mc.cores=4)
 
 
-power_models <- readRDS('bltest_power.RDS')
+power_models <- readRDS('bltest_models.RDS')
 
-models <- readRDS('blmodels.RDS')
+models <- readRDS('blmodel.RDS')
 
 ###################################################
 
@@ -23,13 +23,15 @@ power <- function(p){
   print(paste('Computing power: ',p))
   df$bin <- as.numeric(rbernoulli(nrow(df),p))
   
-  bernlognorm_power <- update(bernlognorm,newdata=df)
-  bernlognorm_null_power <- update(bernlognorm_null,newdata=df)
-  bern_null_power <- update(bern_null,newdata=df)
+  bernlognorm_power <- update(power_models$bernlognorm,newdata=df)
+  bernlognorm_null_power <- update(power_models$bernlognorm_null,newdata=df)
+  bern_null_power <- update(power_models$bern_null,newdata=df)
   c(p,post_prob(bernlognorm_power,bernlognorm_null_power,bern_null_power))
 }
 
-power_seq <- map(seq(.05,.95,.05),power)
+# power_seq <- map(seq(.80,.95,.01),power)
+power_seq <- readRDS('bltest_power.RDS')
+power_seq[[7]] = power(.86)
 
 saveRDS(power_seq,file='bltest_power.RDS')
 #############################################################
